ce2ecd527e540c6a390299d8b2626f13706ab8f4
Okemwag <gabrielokemwa83@gmail.com>
2022-03-16 14:04:41 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,12 +7,20 @@ include "main.h"
 */
 int _abs(int c)
 {
-	if (c < 0)
-	{
-	 	return (c * -1);
-	}
-	else
-	}
-		return (c);
-	}
+
+if (c < 0)
+{
+return (c * ( -1));
+}
+
+else if (c == 0)
+{
+return (0);
+}
+
+else
+{
+return (c);
+}
+
 }
