88d541f3b0e17611ad9bbe73b64d16b58a520f37
Okemwag <gabrielokemwa83@gmail.com>
2022-03-16 13:20:14 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,23 +8,17 @@
 */
 void print_to_98(int n)
 {
-	int i, j;
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d,", n--);
+		printf("%d\n", n);
+	}
 
-	if (n <= 98)
+	else
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-printf("%d, ", i);
-			else if (i == 98)
-printf("%d\n", i);
-		}
-	} else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-printf("%d, ", j);
-			else if (j == 98)
-printf("%d\n", j);
-		}
+		while (n < 98)
+			printf("%d,", n++);
+		printf("%d\n", n);
 	}
 }
