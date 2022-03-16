b09c11078ae044a081aaf100f6516b4aadfd30a1
Okemwag <gabrielokemwa83@gmail.com>
2022-03-16 10:16:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+* add - adds two integers and returns the results
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
