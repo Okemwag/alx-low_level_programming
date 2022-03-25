e0b7264884907fc70b73d8be43514c0a511da500
Okemwag <gabrielokemwa83@gmail.com>
2022-03-25 09:58:33 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,39 +7,21 @@
 */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int sign = 1;
+	unsigned int num = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-	while (s[len] != '\0')
-		len++;
+		else if (*s >= '0' && *s <= '9')
 
-	while (i < len && f == 0)
-	{
-		if (s[i] == '_')
-			++d;
+			num = (num * 10) + (*s - '0');
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
- digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-			f = 0;
-		}
-		i++;
+		else if (num > 0)
+			break;
 	}
+	while (*s++);
 
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (num * sign);
 }
