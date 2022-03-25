3e9fbf3eb0b25998c8237fd2cc3c55f42b58eaef
Okemwag <gabrielokemwa83@gmail.com>
2022-03-25 08:45:17 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,19 @@
 
 void _puts(char *str)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (count >= 0)
 	{
-		-putchar(str[i]);
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
+}
+
 	_putchar('\n');
 }
