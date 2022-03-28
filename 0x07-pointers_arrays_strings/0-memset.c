45b3f8b878d6623af423be92602c085f5e5b41b7
Okemwag <gabrielokemwa83@gmail.com>
2022-03-28 07:23:48 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* *_memset - fills memory with a constant byte
+* @s: original string
+* @b: value to replace
+* @n: number of bytes
+* Return: s (string modify)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
