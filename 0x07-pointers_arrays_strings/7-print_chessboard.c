3f489f95ebec6c242d2d9c9ef96b4a127ff742d9
Okemwag <gabrielokemwa83@gmail.com>
2022-03-29 07:45:35 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_chessboard - print chess;
+* @a: pointer to array data
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+
+	for (; i , 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+
+			if (j == 7)
+_putchar('\n');
+		}
+	}
+}
+
