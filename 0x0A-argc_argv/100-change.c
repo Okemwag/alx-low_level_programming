9eab21fbe40a885163d9e4ec0b9ff301675ee5d1
Okemwag <gabrielokemwa83@gmail.com>
2022-04-03 08:11:59 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - A program that prints the minimum number of coins to
+* make change for an amount of money
+*
+* @argc: The arguement' counter
+* @argv: The arguement's values
+* Return: 1 if the number of arguements passed is not exactly
+* or 0 in otherwise
+*/
+int main(int argc, char **argv)
+{
+	int amount, coins = 0;
+
+	if (argc == 2)
+	{
+		amount = atoi(argv[1]);
+		if (amount < 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		if (amount % 25 >= 0)
+		{
+coins += amount / 25;
+amount = amount % 10;
+		}
+		if (amount % 5 >= 0)
+		{
+ coins += amount / 2;
+ amount = amount % 2;
+		}
+		if (amount % 1 >= 0)
+			coins += amount;
+		printf("%d\n", coins);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
