ac965289d47bcc4c513b762bdc5d102350f472cb
Okemwag <gabrielokemwa83@gmail.com>
2022-04-12 22:13:08 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+* struct dog - information about dogs
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+* dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
