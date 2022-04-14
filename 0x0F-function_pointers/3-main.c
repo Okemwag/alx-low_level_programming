799901e8352b92669c79ab801dc79f95bb3868c1
Okemwag <gabrielokemwa83@gmail.com>
2022-04-14 05:52:43 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
 * main - calculator
 * @argc: arguments counter
@@ -9,25 +8,38 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result = 0;
-	int (*operate_func)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
-	operate_func = get_op_func(argv[2]);
-	if (!operate_func)
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	result = operate_func(num1, num2);
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
 	printf("%d\n", result);
-	return (EXIT_SUCCESS);
+
+	return (0);
 }
