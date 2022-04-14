64596b272e304b6734fb7dad97ef5131d7007d1b
Okemwag <gabrielokemwa83@gmail.com>
2022-04-14 05:57:14 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
 * struct op - Struct op
 * @op: The operator
@@ -7,7 +12,7 @@
 */
 typedef struct op
 {
-	char op;
+	char *op;
 	int (*f)(int a, int b);
 } op_t;
 
@@ -18,4 +23,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 #endif
