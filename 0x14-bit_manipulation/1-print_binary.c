6884f322d214376317fab88ac6f603f92f89c080
Okemwag <gabrielokemwa83@gmail.com>
2022-04-29 12:15:38 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_binary - main function
+* @n: The number to convert.
+* Description: This function prints the binary representation of a number.
+* Return: Nothing
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
