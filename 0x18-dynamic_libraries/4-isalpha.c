70c09c97a3c12238b1910b9ff89f211a92e59763
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-16 19:58:27 +0300

    Add files via upload

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - verify is alphabetic characters.
+ * @c: character
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
